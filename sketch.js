1b459ca5897f81dc239989c4fe3294701afc656c
Jake Priddy <priddyjacob84@gmail.com>
2016-08-15 23:39:27 -0400

    8/15 1139

--- sketch.js
+++ sketch.js
@@ -16,17 +16,10 @@ function draw() {
   }
 } 
 
-
-
-
-
-
-
-
 function keyPressed() {
   if (keyCode === RIGHT_ARROW) {
     ship.move(1);
   } else if (keyCode === LEFT_ARROW) {
     ship.move(-1);
   }
-  }
+}
